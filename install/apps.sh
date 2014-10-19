612b5bd57a4b0f4c5858e88b0526c538c0db4da8
matt sullivan <mcsullivan@Admins-MacBook-Pro.local>
2014-10-19 14:18:54 -0700

    update .gitconfig and apps.sh

--- install/apps.sh
+++ install/apps.sh
@@ -1,4 +1,13 @@
 ok brew # Ensure homebrew is installed
-ok cask # Ensure cask is installed
+ok brew brew-cask  # Ensure cask is installed
 
+# tools
 ok cask iterm2
+
+# fun
+ok cask rdio
+ok cask minecraft
+
+# storage
+ok cask dropbox
+ok cask onepassword
